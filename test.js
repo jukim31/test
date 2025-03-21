a5f0f5ef7ee7490441024f2de2efbdb442a3e86c
jukim31 <jukim31@yahoo.com>
2025-03-21 15:05:51 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,14 @@
+console.log("--------------");
+console.log("Hi there!");
+console.log("--------------");
+
+// Log a message to the console
+console.log("Welcome to the JavaScript page!");
+
+// Function to display an alert
+function showAlert() {
+    console.error("Hello! This is a basic JavaScript alert!!!!");
+}
+
+// Call the function
+showAlert();
