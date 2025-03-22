a9a711ac9ce6cfdfa134f5196033aadede296172
jukim31 <jukim31@yahoo.com>
2025-03-22 14:09:06 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -2,13 +2,16 @@ console.log("--------------");
 console.log("Hi there!");
 console.log("--------------");
 
-// Log a message to the console
-console.log("Welcome to the JavaScript page!");
+let button = document.getElementById("btn_id1");
+
+button.addEventListener("click", showAlert);
 
 // Function to display an alert
 function showAlert() {
-    console.error("Hello! This is a basic JavaScript alert!!!!");
+    let newWin = window.open("", "My Popup", "width=300,height=200");
+    //newWin.document.write("<h1>Hello, world!</h1>");
+//    console.error("Hello! This is a basic JavaScript alert!!!!");
 }
 
 // Call the function
-showAlert();
+// showAlert();
